588b0e7299181af38f6face92140483a2b9ae188
buildveloper <christopherlegacy01@gmail.com>
2025-07-06 14:06:38 +0100

    Moved Supabase keys to environment variables

--- frontend/src/supabaseClient.ts
+++ frontend/src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gayxijgndtkpjnbiokys.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdheXhpamduZHRrcGpuYmlva3lzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1NzM0NDQsImV4cCI6MjA2NzE0OTQ0NH0.BdTPoCCoyy0D0pnaIloLeQczAu0j0tSCKgZthURDqdo';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
